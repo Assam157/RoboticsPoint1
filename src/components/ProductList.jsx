08d214c4f54da2bbbbbb5ba7cd851f53af5de07d
Assam157 <asasm8106@gmail.com>
2025-01-22 21:09:42 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -16,7 +16,7 @@ const ProductList = () => {
     useEffect(() => {
   const fetchProducts = async () => {
     try {
-        const response = await fetch('https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/api/products', {
+        const response = await fetch('https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/api/products', {
             method: 'GET', // Request method
             headers: {
                 'Content-Type': 'application/json',
