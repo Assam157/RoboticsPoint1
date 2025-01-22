aad4606ecb9ea0edcac066cd7cd5d657873f8993
Assam157 <asasm8106@gmail.com>
2025-01-22 21:09:09 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch("https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/login", {
+            const response = await fetch("https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
