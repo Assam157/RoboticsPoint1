d9e988c702819923ccb79745671d110fb227275f
Assam157 <asasm8106@gmail.com>
2025-01-22 21:10:03 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch("https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/signup", {
+            const response = await fetch("https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
